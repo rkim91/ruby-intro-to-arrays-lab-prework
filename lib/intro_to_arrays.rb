323ac1178d7b5c8a9b7a39e240e7e2938065272c
Richard Kim <richeekim@gmail.com>
2018-03-28 23:13:01 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,31 @@
+def instantiate_new_array
+  @my_new_array = []
+end
+
+def array_with_two_elements
+  @my_two_array = [1,2]
+end
+
+def first_element(array)
+  @taylor_swift[0]
+end
+
+def third_element(array)
+  @taylor_swift[2]
+end
+
+def last_element(array)
+  @taylor_swift[-1]
+end
+
+def first_element_with_array_methods(x)
+  @south_east_asia.shift
+end
+
+def last_element_with_array_methods(x)
+  @south_east_asia.pop
+end
+
+def length_of_array(x)
+  @programming_languages.length
+end
